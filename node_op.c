42914bc092e0dc5065b042776f4c53aed2dddcb7
sebteouii <sebteouiikhoubayb@gmail.com>
2024-01-08 22:23:40 +0100

    handl file

---
+++ node_op.c
@@ -0,0 +1,65 @@
+#include "monty.h"
+stack_t *head = NULL;
+/**
+ * create_node - Creates a node.
+ * @n: Number to go inside the node.
+ * Return: a pointer to the node.
+ */
+stack_t *create_node(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
+/**
+ * add_node - Adds a node to the queue.
+ * @new_node: Pointer to the new node.
+ *  Return: void
+ */
+
+void add_node(stack_t **new_node)
+{
+	stack_t *tmp;
+
+	if (new_node == NULL || *new_node == NULL)
+		exit(EXIT_FAILURE);
+	if (head == NULL)
+	{
+		head = *new_node;
+		return;
+	}
+	tmp = head;
+	while (tmp->next != NULL)
+		tmp = tmp->next;
+
+	tmp->next = *new_node;
+	(*new_node)->prev = tmp;
+}
+
+/**
+ * free_nodes - Frees nodes in the stack.
+ * Return: void
+ */
+void free_nodes(void)
+{
+	stack_t *p;
+
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		p = head;
+		head = head->next;
+		free(p);
+	}
+}
