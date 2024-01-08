37384d422e89eadae403167335cc25bfa57bc4b6
khalilBahi <7753@holbertonstudents.com>
2024-01-08 23:06:08 +0100

    find the corect op

--- find.c
+++ find.c
@@ -4,15 +4,13 @@
  * @opcode: opcode
  * @value: argument of opcode
  * @line_number: line number
- * @sp_line:  storage sp_line. If 0 Nodes will be entered as a stack
+ * @sp_line:  storage sp_line
  */
 void fun_found(char *opcode, char *value, int line_number, int sp_line)
 {
-	int i;
-	int kk;
+	int i, kk;
 
-	// Initialize the array of function names and pointers to functions
-	instruction_t op_list[] = {
+	instruction_t opc_list[] = {
 		{"push", _push},
 		{"pall", _pall},
 		{"pint", _pint},
@@ -21,18 +19,16 @@ void fun_found(char *opcode, char *value, int line_number, int sp_line)
 		{"swap", _swap},
 		{"nop", _nop},
 		{NULL, NULL}};
-	// Loop through the function list and find the matching function
-	for (kk = 1, i = 0; op_list[i].opcode != NULL; i++)
+
+	for (kk = 1, i = 0; opc_list[i].opcode != NULL; i++)
 	{
-		if (strcmp(opcode, op_list[i].opcode) == 0)
+		if (strcmp(opcode, opc_list[i].opcode) == 0)
 		{
-			// Call the function and pass the required parameters
-			check_op(op_list[i].f, opcode, value, line_number, sp_line);
+			check_op(opc_list[i].f, opcode, value, line_number, sp_line);
 			kk = 0;
 		}
 	}
 
-	// If no matching function is found, print an error message and exit the program
 	if (kk == 1)
 	{
 		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
